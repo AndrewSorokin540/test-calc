5383e0766fabde5df0a0c8d6297321bd85096f34
Andrey Sorokin <andrewsorokin540@gmail.com>
2019-11-25 01:24:00 +0700

    just commit

--- src/components/InvestmentSumm/InvestmentSumm.jsx
+++ src/components/InvestmentSumm/InvestmentSumm.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Form } from 'reactstrap';
 import './InvestmentSumm.scss';
+import Numeral from "numeral";
 
 class InvestmentSumm extends React.Component {
 
@@ -11,10 +12,6 @@ class InvestmentSumm extends React.Component {
         };
     }
 
-    handleClick = () => {
-        console.log(1);
-    }
-
     investmentSummArrLastChild = this.props.investmentSummArr[this.props.investmentSummArr.length - 1];
 
     handleInputChange = e => {
@@ -23,7 +20,6 @@ class InvestmentSumm extends React.Component {
         }
         this.setState({ currentInvestmentSumm: (e.currentTarget.value) });
         this.props.updateInvestmentSumm(e.currentTarget.value);
-        console.log(e.currentTarget.value);
     };
 
     render() {
@@ -34,7 +30,7 @@ class InvestmentSumm extends React.Component {
         const sliderticks = this.props.investmentSummArr.map((el, i) => (
             <label key={i} className="rangeLink" style={{ left: `${investScaleStep * el}%` }}>
                 <span className="d-table" >{el / 1000}т.</span>
-                <input hidden type="text" value={`${+el}`} onClick={this.handleInputChange}/>
+                <input hidden type="text" value={`${+el}`} onClick={this.handleInputChange} />
             </label>
         ))
         return (
@@ -42,7 +38,7 @@ class InvestmentSumm extends React.Component {
                 <div className="d-flex align-items-center justify-content-between mb-5">
                     <span>Сумма для инвестирования</span>
                     <input type="text" className="investment-summ__summ rounded-pill bg-white border border-secondary"
-                        value={(this.state.currentInvestmentSumm)}
+                        value={Numeral(this.state.currentInvestmentSumm).format('0,0')}
                         onInput={this.handleInputChange}
                     />
                 </div>
