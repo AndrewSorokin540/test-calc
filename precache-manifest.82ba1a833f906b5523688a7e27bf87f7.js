f129545d1d7c83d7fff3b7b984be63835ac95c2a
Andrey Sorokin <andrewsorokin540@gmail.com>
2019-12-01 17:26:42 +0700

    Updates

---
+++ precache-manifest.82ba1a833f906b5523688a7e27bf87f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210060d7b9d0e189c110957d8e5984e6",
+    "url": "/test-calc/index.html"
+  },
+  {
+    "revision": "6665ccaca7923e4521c4",
+    "url": "/test-calc/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "a3b97ec92fc7240042d4",
+    "url": "/test-calc/static/css/main.1aeba62a.chunk.css"
+  },
+  {
+    "revision": "6665ccaca7923e4521c4",
+    "url": "/test-calc/static/js/2.53cb55c9.chunk.js"
+  },
+  {
+    "revision": "a3b97ec92fc7240042d4",
+    "url": "/test-calc/static/js/main.4870ccd3.chunk.js"
+  },
+  {
+    "revision": "21161971595a37dd25fc",
+    "url": "/test-calc/static/js/runtime-main.614a46aa.js"
+  }
+]);
